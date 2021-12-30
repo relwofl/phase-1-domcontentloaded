7f88a9844e985aa149c6d143ccb983f62e1d957a
Luke Fowler <luciusfow@gmail.com>
2021-12-29 20:31:56 -0500

    commit tree 102bf57f8321a36c85ba63231e6a9ed3f5d1c283

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  domHasLoaded()
+})
+function domHasLoaded(){
+    document.getElementById('text').innerHTML = "This is really cool!"
+}
